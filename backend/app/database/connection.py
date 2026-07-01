adf6949e1916669fc11a6dfaf97cae4f1cf2e244
Ritik Mistry <ritikmistry712@gmail.com>
2026-07-01 10:38:59 +0530

    included docker file to host in render and docker

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./finpilot.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./finpilot.db")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
